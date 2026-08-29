1a868387f79d3fdf33704742feefc1f39aa3804c
Azives <jpsanchessilva@gmail.com>
2026-08-29 19:44:01 -0300

    Add files via upload

---
+++ Exercicios_Fixacao/H5B/H5B/Form3.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace H5B
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.pb_imagem2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_imagem2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_imagem2
+            // 
+            this.pb_imagem2.Image = global::H5B.Properties.Resources.PALMEIRASI2025INFA1;
+            this.pb_imagem2.Location = new System.Drawing.Point(0, 0);
+            this.pb_imagem2.Name = "pb_imagem2";
+            this.pb_imagem2.Size = new System.Drawing.Size(800, 703);
+            this.pb_imagem2.TabIndex = 0;
+            this.pb_imagem2.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pb_imagem2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form3";
+            this.Text = "Manto";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_imagem2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_imagem2;
+    }
+}
