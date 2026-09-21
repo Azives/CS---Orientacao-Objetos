858e3446b1730c395a0ace14f01d019f84e525c1
Azives <jpsanchessilva@gmail.com>
2026-09-20 22:02:00 -0300

    Add files via upload

---
+++ Exercicios_Fixacao/Encapsulamento/Encapsulamento/Encaps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    internal class Encaps
+    {
+        static void Main()
+        {
+            Modelar c1 = new Modelar();
+            c1.Numero = 1;
+            c1.Saldo = 1000;
+            c1.Limite = 500;
+
+            Modelar c2 = new Modelar();
+            c2.Numero = 2;
+            c2.Saldo = 250;
+            c2.Limite = 100;
+
+            c2.Saldo = 400;
+
+            System.Console.WriteLine(c1.Numero + " | " + c1.Saldo + " | " + c1.Limite);
+            System.Console.WriteLine(c2.Numero + " | " + c2.Saldo + " | " + c2.Limite);
+        }
+    }
+}
---
+++ Exercicios_Fixacao/Encapsulamento/Encapsulamento/Modelar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+
+    class Modelar
+    {
+        private int numero;
+        private double saldo;
+        private double limite;
+
+        public int Numero
+        {
+            get
+            {
+                return this.numero;
+            }
+            set
+            {
+                this.numero = value;
+            }
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return this.saldo;
+            }
+            set
+            {
+                this.saldo = value;
+            }
+        }
+
+        public double Limite
+        {
+            get
+            {
+                return this.limite;
+            }
+            set
+            {
+                this.limite = value;
+            }
+        }
+    }
+}
